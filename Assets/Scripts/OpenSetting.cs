8d6e6bedc97e0b84d03d39b4bfd77de6deb6594e
yorickili <yorickili@tencent.com>
2018-09-18 18:58:57 +0800

    add hanlder

---
+++ Assets/Scripts/OpenSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenSetting : MonoBehaviour {
+
+    public GameObject Setting;
+
+	void Start ()
+    {
+        GetComponent<Button>().onClick.AddListener(Open);
+    }
+
+    void Open ()
+    {
+        Instantiate(Setting, new Vector3(0, 0, 0), this.transform.rotation);
+    }
+}
